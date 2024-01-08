04ac13acd85317feb8195855a87db8e2b7629920
Nolwenn123 <nolwenn.bourey@gmail.com>
2024-01-08 13:45:30 +0100

    etape 3

--- exercice.java
+++ exercice.java
@@ -12,48 +12,34 @@ public class VérificationLangueTest {
 
     @ParameterizedTest
     @MethodSource("casTestBienDit")
-    @DisplayName("ETANT DONNE un utilisateur parlant une langue, QUAND on entre un palindrome, ALORS il est renvoyé ET le <bienDit> de cette langue est envoyé")
-    public void testPalindromeAvecBienDit(String chaîne, LangueInterface langue, String bienDit) {
-        var vérificateur = new VérificationPalindromeBuilder()
-                .AyantPourLangue(langue)
-                .Build();
-
-        // QUAND on vérifie si c'est un palindrome
-        String résultat = vérificateur.Vérifier(chaîne);
-
-        // ALORS la chaîne est renvoyée, suivie de « Bien dit » dans cette langue
-        String attendu = chaîne + System.lineSeparator() + bienDit;
-        assertTrue(résultat.contains(attendu));
-    }
-
-    @ParameterizedTest
-    @MethodSource("casTestBonjour")
-    @DisplayName("ETANT DONNE un utilisateur parlant une langue, QUAND on saisit une chaîne, ALORS <bonjour> de cette langue est envoyé avant tout")
-    public void testBonjour(String chaîne, LangueInterface langue, String bonjour) {
+    @DisplayName("ETANT DONNE un utilisateur parlant une langue ET que la période de la journée est <période>, QUAND on saisit une chaîne, ALORS <salutation> de cette langue à cette période est envoyé avant tout")
+    public void testBonjour(String chaîne, LangueInterface langue, MomentDeLaJournée momentDeLaJournée, String salutation) {
         var vérification = new VérificationPalindromeBuilder()
                 .AyantPourLangue(langue)
+                .AyantPourMomentDeLaJournée(momentDeLaJournée)
                 .Build();
 
         // QUAND on vérifie si c'est un palindrome
         String résultat = vérification.Vérifier(chaîne);
 
-        // ALORS toute réponse est précédée de <bonjour> dans cette langue
+        // ALORS toute réponse est précédée de <salutation> dans cette langue à cette période de la journée
         String[] lines = résultat.split(System.lineSeparator());
-        assertEquals(bonjour, lines[0]);
+        assertEquals(salutation, lines[0]);
     }
 
     @ParameterizedTest
     @MethodSource("casTestAuRevoir")
-    @DisplayName("ETANT DONNE un utilisateur parlant une langue, QUAND on saisit une chaîne, ALORS <auRevoir> dans cette langue est envoyé en dernier")
-    public void testAuRevoir(String chaîne, LangueInterface langue, String auRevoir) {
+    @DisplayName("ETANT DONNE un utilisateur parlant une langue ET que la période de la journée est <période>, QUAND on saisit une chaîne, ALORS <auRevoir> dans cette langue à cette période est envoyé en dernier")
+    public void testAuRevoir(String chaîne, LangueInterface langue, MomentDeLaJournée momentDeLaJournée, String auRevoir) {
         var vérification = new VérificationPalindromeBuilder()
                 .AyantPourLangue(langue)
+                .AyantPourMomentDeLaJournée(momentDeLaJournée)
                 .Build();
 
         // QUAND on vérifie si c'est un palindrome
         String résultat = vérification.Vérifier(chaîne);
 
-        // ALORS toute réponse est suivie de <auRevoir> dans cette langue
+        // ALORS toute réponse est suivie de <auRevoir> dans cette langue à cette période de la journée
         String[] lines = résultat.split(System.lineSeparator());
         String lastLine = lines[lines.length - 1];
         assertEquals(auRevoir, lastLine);
@@ -61,28 +47,19 @@ public class VérificationLangueTest {
 
     static Stream<Arguments> casTestBienDit() {
         return Stream.of(
-                Arguments.of("radar", new LangueFrançaise(), "Bien dit"),
-                Arguments.of("level", new LangueAnglaise(), "Well said"),
-                Arguments.of("test", new LangueEspagnole(), "Bien dicho")
-                // Ajoutez d'autres cas de test avec différentes langues et <bienDit> correspondant
-        );
-    }
-
-    static Stream<Arguments> casTestBonjour() {
-        return Stream.of(
-                Arguments.of("test", new LangueFrançaise(), "Bonjour"),
-                Arguments.of("radar", new LangueAnglaise(), "Hello"),
-                Arguments.of("prueba", new LangueEspagnole(), "Hola")
-                // Ajoutez d'autres cas de test avec différentes langues et <bonjour> correspondant
+                Arguments.of("radar", new LangueFrançaise(), MomentDeLaJournée.Matin, "Bonjour_am"),
+                Arguments.of("level", new LangueAnglaise(), MomentDeLaJournée.AprèsMidi, "Bonjour_pm"),
+                Arguments.of("test", new LangueEspagnole(), MomentDeLaJournée.Soirée, "Bonjour_soir")
+                // Ajoutez d'autres cas de test avec différentes langues, périodes de la journée, et <salutation> correspondant
         );
     }
 
     static Stream<Arguments> casTestAuRevoir() {
         return Stream.of(
-                Arguments.of("test", new LangueFrançaise(), "Au revoir"),
-                Arguments.of("radar", new LangueAnglaise(), "Goodbye"),
-                Arguments.of("prueba", new LangueEspagnole(), "Adiós")
-                // Ajoutez d'autres cas de test avec différentes langues et <auRevoir> correspondant
+                Arguments.of("test", new LangueFrançaise(), MomentDeLaJournée.Matin, "AuRevoir_am"),
+                Arguments.of("radar", new LangueAnglaise(), MomentDeLaJournée.AprèsMidi, "AuRevoir_pm"),
+                Arguments.of("prueba", new LangueEspagnole(), MomentDeLaJournée.Soirée, "AuRevoir_soir")
+                // Ajoutez d'autres cas de test avec différentes langues, périodes de la journée, et <auRevoir> correspondant
         );
     }
 }
